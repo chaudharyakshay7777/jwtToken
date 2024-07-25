76e3fb5c41f71c4ed5d5f67823932ba8c7554e8f
akshay.chaudhary <akshay.chaudhary@verse.in>
2024-07-25 21:30:01 +0530

    w.i.p auth API | security config

---
+++ src/main/java/in/hey/jwttoken/auth/AuthenticationService.java
@@ -0,0 +1,37 @@
+package in.hey.jwttoken.auth;
+
+import in.hey.jwttoken.config.JwtService;
+import in.hey.jwttoken.user.Role;
+import in.hey.jwttoken.user.User;
+import in.hey.jwttoken.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
---
+++ src/main/java/in/hey/jwttoken/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package in.hey.jwttoken.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
